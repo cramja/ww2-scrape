2bd5fe5dfd89347495c311437c9eece65b08db1b
cramja <marc.spehlmann@gmail.com>
2022-02-11 11:27:37 -0800

    improve initial scrape

--- server/wiki.py
+++ server/wiki.py
@@ -1,3 +1,4 @@
+import datetime
 import hashlib
 import json
 import logging
@@ -9,6 +10,8 @@ from typing import List
 import bs4
 import firebase_admin
 import requests
+from bs4 import NavigableString
+from bs4 import Tag
 from firebase_admin import credentials
 from firebase_admin import firestore
 
@@ -35,12 +38,54 @@ def _get_doc(url: str) -> str:
     return res.text
 
 
-def _scrape_wiki_for_dates(html) -> dict:
-    # TODO: handle li variant
+def _parse_date_tag(tag, submit):
+    # date tags can contain multiple events. They're <br> separated
+    def clean(text):
+        return re.sub(r"^[0-9]*(-[0-9]+)?\W*:\W*", "", re.sub(r"\[\d+\]", "", text)).strip()
+    start_day, text = tag.text.split(":", 1)
+    end_day = None
+    if "-" in start_day:
+        start_day, end_day = start_day.split("-")
 
-    months = ["January", "February", "March", "April", "May", "June", "July",
-              "August", "September", "October", "November", "December"]
-    months = {month: 1 + i for i, month in enumerate(months)}
+    links = []
+    text = ""
+    for ele in tag.contents:
+        if isinstance(ele, NavigableString):
+            text += str(ele)
+        elif ele.name == "a":
+            rel = ele.get("href")
+            if rel.startswith("/wiki/"):
+                links.append(f"https://en.wikipedia.org{rel}")
+            text += ele.text
+        elif ele.name == "br":
+            submit(start_day, end_day, clean(text), links)
+            links = []
+            text = ""
+        else:
+            text += ele.text
+    if text.strip():
+        submit(start_day, end_day, clean(text), links)
+
+
+def _parse_event_tag(tag, submit):
+    # singular events in dd, list of events in ul (search for li's)
+    if not isinstance(tag, Tag):
+        return None
+    event_html = [*tag.find_all("dd"), *tag.find_all("li")]
+    for tag in event_html:
+        try:
+            _parse_date_tag(tag, submit)
+        except:
+            # TODO: handle inline elements
+            LOG.debug(f"malformed line: {tag.text}")
+
+
+def _scrape_wiki_for_dates(html):
+    scrape_time = datetime.datetime.now().isoformat()
+
+    months = {month: 1 + i for i, month in enumerate([
+        "January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"])}
 
     doc = bs4.BeautifulSoup(html, 'html.parser')
     container = doc.find(id="mw-content-text").contents[0]
@@ -48,6 +93,7 @@ def _scrape_wiki_for_dates(html) -> dict:
 
     year = int(doc.find(id="firstHeading").text[-5:][:-1])
     month = None
+
     for tag in container.contents:
         if month is None:
             if tag.name == 'h2':
@@ -58,33 +104,18 @@ def _scrape_wiki_for_dates(html) -> dict:
             else:
                 break
         else:
-            event_html = tag.find("dd")
-            if event_html != -1:
-                try:
-                    start_day, text = tag.text.split(":", 1)
-                    end_day = None
-                    if "-" in start_day:
-                        start_day, end_day = start_day.split("-")
-                    start_day = f"{year}-{months[month]:02d}-{int(start_day):02d}"
-                    end_day = f"{year}-{months[month]:02d}-{int(end_day):02d}" if end_day else start_day
+            def submit(start, end, text, links):
+                start = f"{year}-{months[month]:02d}-{int(start):02d}"
+                events.append({
+                    "id": hashlib.md5(text.encode()).hexdigest()[:12],
+                    "startDate": start,
+                    "endDate": f"{year}-{months[month]:02d}-{int(end):02d}" if end else start,
+                    "text": text,
+                    "links": links,
+                    "createTime": scrape_time
+                })
 
-                    links = []
-                    for link in tag.find_all("a"):
-                        ref = link.get("href")
-                        if ref.startswith("/wiki/"):
-                            links.append(f"https://en.wikipedia.org/{ref}")
-
-                    text = re.sub(r"\[\d+\]", "", text.strip())
-                    events.append({
-                        "id": hashlib.md5(text.encode()).hexdigest()[:12],
-                        "startDate": start_day,
-                        "endDate": end_day,
-                        "text": text,
-                        "links": links
-                    })
-                except:
-                    # TODO: handle inline elements
-                    LOG.debug(f"malformed line: {tag.text}")
+            _parse_event_tag(tag, submit)
 
     return events
 
@@ -94,18 +125,19 @@ def scrape_wiki():
     events = []
     for url in urls:
         events.extend(_scrape_wiki_for_dates(_get_doc(url)))
-    return json.dumps(events, indent=2)
+    return events
 
 
-def sync_firestore(cert, events_file):
+def sync_firestore(cert, events):
     """
     https://firebase.google.com/docs/firestore/quickstart?authuser=0#python
     """
     app = firebase_admin.initialize_app(credentials.Certificate(cert))
     db = firestore.client(app)
 
-    with open(events_file, 'r') as f:
-        events = json.load(f)
+    if isinstance(events, str):
+        with open(events, 'r') as f:
+            events = json.load(f)
 
     updated, checked = 0, 0
     for event in events:
@@ -118,3 +150,7 @@ def sync_firestore(cert, events_file):
             checked += 1
         if (updated + checked) % 10 == 0:
             print(f"updated {updated} and checked {checked} of {len(events)}")
+
+
+if __name__ == "__main__":
+    sync_firestore("/home/marc/workspace/scrape-wiki/server/event-editor-firebase-adminsdk-vtwqr-3a052c189f.json", scrape_wiki())
